36cd4138e6c9ecedecdfa03d849d41574f588f0b
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-26 20:00:08 -0700

    not really working

--- jQueryFunctions.js
+++ jQueryFunctions.js
@@ -1,3 +1,28 @@
+var isVisable = true;
+
 $('#clickButton').click(function(){
-    alert("You have clicked a button!");
+    $('div').addClass("divclass");
+    $('button').addClass("buttonclass")
+});
+
+$('#hideButton').click(function(){
+    isVisable = false;
+    $('#toHide').hide();
+});
+
+
+$('#showButton').click(function(){
+    if(isVisable === true){
+        alert("The div is not hidden");
+    }
+    else{
+        isVisable = true;
+        $('#toHide').show();
+    }
+    
+});
+$('#toggleButton').click(function(){
+
+        $('#lightOff').hide();
+
 });
